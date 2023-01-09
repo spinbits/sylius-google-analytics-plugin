3c600124c53de57196e63c3e81780c2a4a105d69
Jakub Lech <poczta.jakublech@gmail.com>
2023-01-09 16:41:03 +0100

    add tests

--- tests/Unit/Factory/RenderHeadTwigFactoryTest.php
+++ tests/Unit/Factory/RenderHeadTwigFactoryTest.php
@@ -13,6 +13,8 @@ namespace Tests\Spinbits\SyliusGoogleAnalytics4Plugin\Unit;
 use Spinbits\SyliusGoogleAnalytics4Plugin\Factory\RenderHeadTwigFactory;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Core\Model\Channel;
 use Twig\Environment;
 
 class RenderHeadTwigFactoryTest extends TestCase
@@ -23,12 +25,25 @@ class RenderHeadTwigFactoryTest extends TestCase
     /** @var MockObject|Environment */
     private MockObject $twig;
 
+    /** @var MockObject|ChannelContextInterface */
+    private MockObject $channelContext;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->twig = $this->createMock(Environment::class);
-        $this->sut = new RenderHeadTwigFactory($this->twig, 'id', 'param', 'template', true);
+        $this->channelContext = $this->createMock(ChannelContextInterface::class);
+
+        $this->sut = new RenderHeadTwigFactory(
+            $this->channelContext,
+            $this->twig,
+            'id',
+            'param',
+            'template',
+            true,
+            ['CHANNEL' => 'G-123123123']
+        );
     }
 
     public function testRenderEnabled()
@@ -45,7 +60,7 @@ class RenderHeadTwigFactoryTest extends TestCase
 
     public function testRenderDisabled()
     {
-        $this->sut = new RenderHeadTwigFactory($this->twig, '', '', '', false);
+        $this->sut = new RenderHeadTwigFactory($this->channelContext, $this->twig, '', '', '', false);
         $this->twig
             ->expects($this->never())
             ->method('render')
@@ -53,5 +68,49 @@ class RenderHeadTwigFactoryTest extends TestCase
 
         $result = $this->sut->render();
         $this->assertSame('', $result);
+    }
+
+    public function testRenderWithDefaultGtagId()
+    {
+        $channel = $this->createMock(Channel::class);
+        $channel->expects($this->once())
+            ->method('getCode')
+            ->willReturn('OTHER_CHANNEL');
+
+        $this->channelContext->expects($this->once())
+            ->method('getChannel')
+            ->willReturn($channel);
+
+        $this->twig
+            ->expects($this->once())
+            ->method('render')
+            ->with(...['template', [
+                'id' => 'id',
+                'url_suffix' => '&param'
+            ]])
+            ->willReturn('example content');
+
+        $this->sut->render();
+    }
+
+    public function testRenderWithDedicatedGtagId()
+    {
+        $channel = $this->createMock(Channel::class);
+        $channel->expects($this->once())
+            ->method('getCode')
+            ->willReturn('CHANNEL');
+
+        $this->channelContext->expects($this->once())
+            ->method('getChannel')
+            ->willReturn($channel);
+
+        $this->twig->expects($this->once())
+            ->method('render')
+            ->with(...['template', [
+                'id' => 'G-123123123',
+                'url_suffix' => '&param'
+            ]]);
+
+        $this->sut->render();
     }
 }
